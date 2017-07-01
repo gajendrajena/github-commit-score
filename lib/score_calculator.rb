5fff687a734d4cafcd88a04438c287150f1239c0
Gajendra Jena <gjena@ctepl.com>
2017-07-01 11:58:10 +0530

    #1-dhh-score
     - using enumerable

--- lib/score_calculator.rb
+++ lib/score_calculator.rb
@@ -1,26 +1,20 @@
 class ScoreCalculator
+  include Enumerable
 
   SCORE_MAP = {
-    "IssuesEvent" => 7,
-    "IssueCommentEvent" => 6,
-    "PushEvent" => 5,
-    "PullRequestReviewCommentEvent" => 4,
-    "WatchEvent" => 3,
-    "CreateEvent" => 2
+  	"IssuesEvent"=> 7,
+  	"IssueCommentEvent"=> 6,
+  	"PushEvent"=> 5,
+  	"PullRequestReviewCommentEvent"=> 4,
+  	"WatchEvent"=> 3,
+  	"CreateEvent"=> 2
   }
   SCORE_MAP.default = 1
 
   def self.execute commits
-    score = 0
-    return score if commits.nil? || commits.empty?
-    scores = commits.collect do|commit|
-      if commit.id
-        SCORE_MAP[commit.type]
-      else
-        0
-      end
-    end
-
-    score = scores.inject(0){|sum,x| sum + x }
+    return 0 if commits.nil? || commits.empty?
+    scores = commits.collect {|commit| SCORE_MAP[commit.type] if commit.id }
+    scores.compact.sum
   end
+
 end
